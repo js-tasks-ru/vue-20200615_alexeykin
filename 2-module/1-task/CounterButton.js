5f74353a28f90a11cc9515c3a3b87ce6bf170d6f
Lapushynski <a.lapushynski@aisnovations.com>
2020-06-26 22:56:49 +0300

    Added increasing button

--- 2-module/1-task/CounterButton.js
+++ 2-module/1-task/CounterButton.js
@@ -1,10 +1,27 @@
 export const CounterButton = {
   // Шаблон потребуется отредактировать
-  template: '<button type="button"></button>',
+  template:
+    '<button type="button" @click="change">{{ count }}</button>',
 
   // Компонент должен иметь пропс
+  props: {
+    count: {
+      type: Number,
+      required: false,
+      default: 0,
+    },
+  },
 
   // Компонент должен иметь модель
+  model: {
+    prop: 'count',
+    event: 'increment',
+  },
 
   // Шаблон лучше иметь максимально простым, а логику выносить в методы
+  methods: {
+    change() {
+      this.$emit('increment', this.count + 1);
+    },
+  },
 };
